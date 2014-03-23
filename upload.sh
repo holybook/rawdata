79fdc3c2e03bebee52e9ff1ae878dfe99607e7bd
Hannes Widmoser <widmoser@gmail.com>
2014-03-23 20:12:50 +0100

    upload script allows arbitrary number of files

--- upload.sh
+++ upload.sh
@@ -1,1 +1,6 @@
-curl "http://localhost:8983/solr/books_en/update?commit=true&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$1
+for file in "$@"
+do
+    echo "Uploading $file..."
+    curl -s -o /dev/null -w "%{http_code}\n" "http://localhost:8983/solr/books_en/update?commit=true&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$file
+done
+

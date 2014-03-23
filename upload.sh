8d63b76edead07c2fe50aa5d275886e274f40e33
Hannes Widmoser <widmoser@gmail.com>
2014-03-23 20:15:49 +0100

    customizable host and core in upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,19 @@
+host=$SOLR_HOST
+core=$SOLR_CORE
+
+if [[ -z $host ]]; then
+  echo "Could not find environment variable SOLR_HOST using default";
+  host="http://localhost:8983"
+fi
+
+if [[ -z $core ]]; then
+  echo "Could not find environment variable SOLR_CORE using default";
+  core="books_en"
+fi
+
 for file in "$@"
 do
     echo "Uploading $file..."
-    curl -s -o /dev/null -w "%{http_code}\n" "http://localhost:8983/solr/books_en/update?commit=true&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$file
+    curl -s -o /dev/null -w "%{http_code}\n" "$host/solr/$core/update?commit=true&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$file
 done
 

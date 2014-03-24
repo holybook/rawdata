1626cd005a322a671e2129d9a9f2bb72fa78a75d
Hannes Widmoser <widmoser@gmail.com>
2014-03-24 01:22:36 +0100

    automatically adding default port

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ core=$SOLR_CORE
 
 if [[ -z $host ]]; then
   echo "Could not find environment variable SOLR_HOST using default";
-  host="http://localhost:8983"
+  host="http://localhost"
 fi
 
 if [[ -z $core ]]; then
@@ -14,8 +14,8 @@ fi
 for file in "$@"
 do
     echo -ne "Uploading $file... "
-    echo `curl -s -o /dev/null -w "%{http_code}\n" "$host/solr/$core/update?commit=false&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$file`
+    echo `curl -s -o /dev/null -w "%{http_code}\n" "$host:8983/solr/$core/update?commit=false&tr=doc.xsl" -H "Content-Type: text/xml" --data-binary @$file`
 done
-echo "Committing..."
-curl -s -o /dev/null -w "%{http_code}\n" "$host/solr/$core/update?commit=true"
+echo -ne "Committing... "
+curl -s -o /dev/null -w "%{http_code}\n" "$host:8983/solr/$core/update?commit=true"
 
